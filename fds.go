c86413b28e2ae686bf8b757adcce7dce331ebb3a
yangqi <1398571658@qq.com>
2020-08-28 15:02:58 +0800

    函数学习

--- fds.go
+++ fds.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	var b, c int
+	b, c = test(2, 3)
+	fmt.Printf("b value is %d, c value is %d", b, c)
+}
+
+func test(a int, b int) (int, int) {
+	//可同时声明并给多个变量复制
+	var c = a + b
+	//变量要么用var声明，要么用运算符 ':=' 直接赋值
+	d := 20
+	if c < 10 {
+		c = d
+	}
+	return c, a
 }
